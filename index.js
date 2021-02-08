f79c538b12c7e296dff1781c8cd990358300884e
salmarakha <61853942+salmarakha@users.noreply.github.com>
2021-02-08 23:46:50 +0200

    comment

--- index.js
+++ index.js
@@ -19,7 +19,6 @@ mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 app.use(express.json());
 app.use('/images', express.static('images'));
 app.use('/users', userRoutes);
-app.use('/blogdetail', auth, commentRoutes);
 app.use('/blogs', auth, blogRoutes);
 app.use('/blogdetail', auth, commentRoutes);
 app.get('/', async (req, res, next) => {
